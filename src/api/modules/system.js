f9597c21f731bb8be819bd9a7a48f770d4cdaf6c
yuanjiayang <yuanjiayang@qiezilife.com>
2018-12-19 17:47:37 +0800

    修改请求method

--- src/api/modules/system.js
+++ src/api/modules/system.js
@@ -4,8 +4,8 @@ import createAPI from '../axios'
 const system = {
     // 用户基本权限
     basic: {
-        userMenu: config => createAPI('/user/menu', 'post', config),
-        userInfo: config => createAPI('/user/info', 'post', config),
+        userMenu: config => createAPI('/user/menu', 'get', config),
+        userInfo: config => createAPI('/user/info', 'get', config),
         userLogin: config => createAPI('/user/login', 'post', config),
         changePwd: config => createAPI('/user/change-pwd', 'post', config),
     },
@@ -13,23 +13,23 @@ const system = {
     config: {
         list: config => createAPI('/admin/configure/list', 'post', config),
         save: config => createAPI('/admin/configure/save', 'post', config),
-        delete: config => createAPI('/admin/configure/del', 'post', config),
+        delete: config => createAPI('/admin/configure/del', 'delete', config),
 
         getParamValue: config => createAPI('/admin/configure/param-value', 'post', config)
     },
     // 菜单管理
     menu: {
-        map: config => createAPI('/menu/father-menus', 'post', config),
+        map: config => createAPI('/menu/father-menus', 'get', config),
         list: config => createAPI('/menu/list', 'post', config),
         save: config => createAPI('/menu/save', 'post', config),
-        delete: config => createAPI('/menu/del', 'post', config),
+        delete: config => createAPI('/menu/del', 'delete', config),
     },
     // 角色管理
     role: {
-        list: config => createAPI('/role/list', 'post', config),
+        list: config => createAPI('/role/list', 'get', config),
         all: config => createAPI('/role/all', 'post', config),
         save: config => createAPI('/role/save', 'post', config),
-        delete: config => createAPI('/role/del', 'post', config),
+        delete: config => createAPI('/role/del', 'delete', config),
         getMenu: config => createAPI('/role/get-menu', 'post', config),
         getUser: config => createAPI('/role/get-user', 'post', config),
         saveUser: config => createAPI('/role/save-user', 'post', config),
@@ -41,7 +41,7 @@ const system = {
     user: {
         list: config => createAPI('/user/list', 'post', config),
         save: config => createAPI('/user/save', 'post', config),
-        delete: config => createAPI('/user/del', 'post', config),
+        delete: config => createAPI('/user/del', 'delete', config),
         resetPwd: config => createAPI('/user/reset-pwd', 'post', config),
     }
 };
