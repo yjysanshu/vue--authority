7bcb105b8e42daf3d792f33656be9f73e1055645
yuanjiayang <yuanjiayang@qiezilife.com>
2019-04-23 21:35:58 +0800

    build

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"development"',
-    BASE_API: '"http://dev.fond.wangyutao.io"',
-    APP_ORIGIN: '"http://dev.fond.wangyutao.io"'
+    BASE_API: '"http://dev"',
+    APP_ORIGIN: '"http://dev"'
 }
